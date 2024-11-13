4608edaf82c12a404ca5d56f2df9973d820edb8d
mikel <mikel.dejuan@educa.madrid.org>
2024-11-13 12:52:18 +0100

    Practica 4

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+echo '<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <title> Practica 4 </title>
+        <meta charset="UTF-8">
+    </head>
+    <body>
+        <h1> Indice </h1>
+        <ol>
+            <li> <a href="Punto1.html"> Punto 1 </a> </li>
+            <li> <a href="Punto2.html"> Punto 2 </a> </li>
+            <li> <a href="Punto3.html"> Punto 3 </a> </li>
+            <li> <a href="Punto4.html"> Punto 4 </a> </li>
+            <li> <a href="Punto5.html"> Punto 5 </a> </li>
+            <li> <a href="Punto6.html"> Punto 6 </a> </li>
+        </ol>
+    </body>
+</html>';
+?>
